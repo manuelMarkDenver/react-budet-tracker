fe66d4cac6bd346e6bf0eb734e279bf6c6545734
manuelMarkDenver <manuel.markdenver@gmail.com>
2023-03-13 21:02:36 +0800

    added create budget func // enhanced ui/ux

--- client/src/helpers.js
+++ client/src/helpers.js
@@ -1,6 +1,32 @@
+export const waait = () =>
+  new Promise((res) => setTimeout(res, Math.random() * 2000));
+
+const generateRandomColor = () => {
+  const existingBudgetLength = fetchData("budgets")?.length ?? 0;
+  return `${existingBudgetLength * 34} 65% 50%`;
+};
+
 // local storage
 export const fetchData = (key) => {
   return JSON.parse(localStorage.getItem(key));
+};
+
+// create budget
+export const createBudget = ({ name, amount }) => {
+  const newItem = {
+    id: crypto.randomUUID(),
+    name: name,
+    amount: +amount,
+    createdAt: Date.now(),
+    color: generateRandomColor(),
+  };
+
+  const existingBudget = fetchData("budgets") ?? [];
+
+  return localStorage.setItem(
+    "budgets",
+    JSON.stringify([...existingBudget, newItem])
+  );
 };
 
 // delete user
