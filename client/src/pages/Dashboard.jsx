2b2a778a7930bd11a1d9bfbd10053872d8f9b68d
manuelMarkDenver <manuel.markdenver@gmail.com>
2023-03-13 19:04:51 +0800

    added form submit func for new accounts

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,23 +1,35 @@
 // rrd imports
 import { useLoaderData } from "react-router-dom";
+import { toast } from "react-toastify";
+
+// components
+import Intro from "../components/Intro";
 
 //  helper functions
-import { fetchData } from "../helpers"
+import { fetchData } from "../helpers";
 
 // loader
 export function dashboardLoader() {
   const userName = fetchData("userName");
-  return { userName }
+  return { userName };
 }
+
+// action
+export const dashboardAction = async ({ request }) => {
+  const data = await request.formData();
+  const formData = Object.fromEntries(data);
+  try {
+    localStorage.setItem("userName", JSON.stringify(formData.userName));
+    return toast.success(`Welcome, ${formData.userName}!`);
+  } catch (error) {
+    console.error(error);
+    throw new Error("There was a problemt creating your account.");
+  }
+};
 
 const Dashboard = () => {
-  const { userName } = useLoaderData()
+  const { userName } = useLoaderData();
 
-  return (
-    <div>
-      <h1>{userName}</h1>
-      Dashboard
-    </div>
-  )
-}
-export default Dashboard
+  return <>{userName ? <p>{userName}</p> : <Intro />}</>;
+};
+export default Dashboard;
